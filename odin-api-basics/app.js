eac0c3171eda7033a036ec4e0197fe27678937e0
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-08-31 18:11:50 +0200

    Set up different methods

--- odin-api-basics/app.js
+++ odin-api-basics/app.js
@@ -10,19 +10,25 @@ const app = express(); // initialize express in app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+/* ------------------------------------------------------------------------------------------------------------ */
 // routes
-app.get('/', (req, res) => {
-  res.send('Received a GET HTTP method');
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
 });
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
 });
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
 });
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
 });
+
+/* ------------------------------------------------------------------------------------------------------------ */
 
 // Start server
 const PORT = process.env.PORT || 3000;
